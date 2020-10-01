2067819c69af814cd88b3fffb43db3af7ff35f11
martinus <martinussitinjak@yahoo.com>
2020-10-01 17:11:45 +0700

    Init Project for Happy Travelling

---
+++ 01_HappyTravelling/src/test/java/kelompok1/_HappyTravelling/ApplicationTests.java
@@ -0,0 +1,13 @@
+package kelompok1._HappyTravelling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
